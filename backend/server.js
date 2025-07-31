b2333d133e881eaa9469b85018dd7add147d94c6
Shivam Tripathi <97305325+shivam0975@users.noreply.github.com>
2025-07-31 19:23:01 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
-const connectDB = require('./config/db');
+import connectDB from './config/db.js';
 const cors = require('cors');
 const { notFound, errorHandler } = require('./middleware/errorMiddleware');
 
