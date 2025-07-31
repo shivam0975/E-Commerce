ab4ba516b1e6cf4be12f7775ceef8211e143ad8d
Shivam Tripathi <97305325+shivam0975@users.noreply.github.com>
2025-07-31 18:44:47 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,32 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
+
+const MONGO_URI = process.env.MONGO_URI;
+
+if (!MONGO_URI) {
+  throw new Error('Missing MONGO_URI');
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
 
 const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.error(`MongoDB connection error: ${error.message}`);
-    process.exit(1);
+  if (cached.conn) {
+    return cached.conn;
   }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+
+    cached.promise = mongoose.connect(MONGO_URI, opts).then(mongoose => mongoose);
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
 };
 
-module.exports = connectDB;
+export default connectDB;
