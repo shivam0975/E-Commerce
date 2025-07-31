5ba902f24a984fdb141e143c9f8361a453cf68fc
Shivam Tripathi <97305325+shivam0975@users.noreply.github.com>
2025-07-31 22:21:19 +0530

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://e-commerce-backend-phi-three.vercel.app/api',
+  baseURL: 'http://localhost:8080/api',
 });
 
 // Add token to headers
