621e2ae4084ce638e7ec40fbc7d97136c8a0c7c9
Shivam Tripathi <97305325+shivam0975@users.noreply.github.com>
2025-08-01 19:29:11 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,31 +7,27 @@ if (!cached) {
 }
 
 const connectDB = async () => {
-  if (cached.conn) {
-    return cached.conn;
-  }
+  if (cached.conn) return cached.conn;
 
   if (!cached.promise) {
     const opts = {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+      bufferCommands: false,
     };
 
     cached.promise = mongoose.connect(process.env.MONGO_URI, opts).then((mongoose) => {
-      console.log('MongoDB Connected');
+      console.log('✅ MongoDB Connected');
       return mongoose;
     });
   }
 
   try {
     cached.conn = await cached.promise;
-  } catch (error) {
+  } catch (e) {
     cached.promise = null;
-    throw error;
+    throw e;
   }
 
   return cached.conn;
 };
 
 module.exports = connectDB;
-
